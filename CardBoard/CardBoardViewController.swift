8d5c63e574f069c12c827ac5d38b6bc3f18e2cda
Nishant Taneja <nishant.taneja2701@gmail.com>
2021-03-08 16:51:43 +0530

    create CardBoardViewController

---
+++ CardBoard/CardBoardViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CardBoardViewController.swift
+//  CardBoard
+//
+//  Created by Nishant Taneja on 08/03/21.
+//
+
+import UIKit
+
+class CardBoardViewController: UIViewController {
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var midView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+}
